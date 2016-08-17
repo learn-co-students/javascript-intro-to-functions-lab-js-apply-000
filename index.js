214936b0fc17757a92a22a037eda52fe65f5113f
Gurwinder Singh <gurwindersinghnyc@gmail.com>
2016-08-17 19:47:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string){
+  if(string.toLowerCase){
+    return "I can't hear you!"
+  }
+  else{
+    "YES INDEED!"
+  }
+}
