c474185ba1a38defa9f7f4e22100c0211c9f0d68
Sally Whang <wsaehee21@outlook.com>
2016-08-19 15:17:35 +0000

    Done.

--- index.js
+++ index.js
@@ -15,28 +15,15 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var lowercase = "hello"
-  string.toLowerCase() === lowercase
-  return "I can\'t hear you!"
+  if (string === string.toLowerCase()){
 
-  // var uppercase = "HELLO"
-  // var mixedCase = "I love you, Grandma."
-  // string.toUpperCase() === uppercase
-  // return "YES INDEED!"
+    return "I can\'t hear you!"
+  }
+  if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }
 
-  // if (string.toLowerCase() === lowercase) {
-  //   return "I can\'t hear you!"
-  // } else if (string.toUpperCase() === uppercase) {
-  //   return "YES INDEED!"
-  // } else if (string.toLowerCase() !== mixedCase && string.toUpperCase() !== mixedCase){
-  //   return "I love you, too."
-  // } else {
-  //   return "say something!"
-  // }
-}
-
-function sayHiToGrandma(string){
-  var uppercase = "HELLO"
-  string.toUpperCase() === uppercase
-  return "YES INDEED!"
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
