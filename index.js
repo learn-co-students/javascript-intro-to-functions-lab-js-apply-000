8065cc99a183b5cc1a3d70caecf6f9157d5d4c26
Noorulain Khaliq <nkhaliq86@gmail.com>
2016-09-03 20:39:12 +0000

    Done.

--- index.js
+++ index.js
@@ -10,18 +10,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can't hear you!";
-  }
-}
-function sayHiToGrandma(string) {
-  if(string === string.toUpperCase()) {
-    return "YES INDEED!";
-  }
-}
-function sayHiToGrandma(string) {
-  if(string === "I love you, Grandma.") {
+  } else if(string === string.toUpperCase()) {
+  	return "YES INDEED!";
+  } else if(string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
