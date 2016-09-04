ac199e095f05f2506b5995d06be0523076caa48a
Barbara Graniello <bgraniello@yahoo.com>
2016-09-04 23:22:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase()
+};
+function whisper(string) {
+  return string.toLowerCase()
+};
+function logShout(string) {
+  console.log(shout(string))};
+function logWhisper(string) {
+    console.log(whisper(string))};
+
+function sayHiToGrandma(string) {
+  If(string = "I love you Grandma.") {
+    console.log("I love you, too.");
+  }
+};
