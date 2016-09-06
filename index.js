6279eafbe2f96e0f2382e29baa68e38a77c591c2
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2016-09-06 18:19:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(hello) {
+  return hello.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return "I can\'t hear you!"
+}
+
+function sayHiToGrandma(STRING) {
+  return "yes indeed!".toUpperCase()
+}
+
+function sayHiToGrandma("I love you, Grandma.") {
+  return "I love you, too."
+}
