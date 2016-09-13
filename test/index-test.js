208f878ccca7195b9b64a6d419e53404587b97c6
Kelly McNevin <kelly.mcnevin@gmail.com>
2016-09-13 20:31:57 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,10 @@
-
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
 
-describe('whisper(string)', function() {
+describe('whisper(string)', function(whisper(string)) {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
