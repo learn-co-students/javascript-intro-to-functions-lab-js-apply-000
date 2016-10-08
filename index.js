6e1e255ec3155ff289f190470bfd67448038fb89
David Booth-Metwally <davidbmetwally@gmail.com>
2016-10-08 23:26:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+
+function shout(string) {
+
+  return string.toUpperCase()
+
+}
+
+function whisper(argument) {
+
+  return argument.toLowerCase()
+
+}
+
+function logShout(string) {
+
+ console.log(string.toUpperCase())
+
+}
+
+function logWhisper(argument) {
+
+console.log(argument.toLowerCase())
+
+  }
+
+  function
+  sayHiToGrandma(string) {
+
+if (string === string.toLowerCase()) {
+return ("I can't hear you!")
+}
+    else if (string === string.toUpperCase()){
+      return ("YES INDEED!")
+    }
+else if (string ==="I love you, Grandma."){
+  return ("I love you, too.")
+}
+
+}
