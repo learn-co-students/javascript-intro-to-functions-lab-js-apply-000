78710ad7803303a95f108d11c24ec7e1e9f8bf36
Crystal Nieves <cnieves90@gmail.com>
2016-10-11 00:49:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function
+  if sayHiToGrandma(whisper)
+    {return ('I can\'t hear you!')}
+  if sayHiToGrandma(shout)
+    {return ('YES INDEED!')}
+}
