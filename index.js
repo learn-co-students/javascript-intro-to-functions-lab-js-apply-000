3613a374eda506f53e2e690d9dd1ffed0a8d9ea4
Tricia Bellot <tbellot06@yahoo.com>
2016-10-19 05:06:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(); string.toUpperCase()
+}
+function logWhisper(string) {
+  console.log(); string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+  var lowercase = "I can't hear you!"
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+  var uppercase = "YES INDEED!"
+}
