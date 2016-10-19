b5e331bd109114aceffa3c031bfc4b00873ce557
Tricia Bellot <tbellot06@yahoo.com>
2016-10-19 15:41:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
-  console.log(); string.toUpperCase()
+  console.log(string.toUpperCase())
 }
+
 function logWhisper(string) {
-  console.log(); string.toLowerCase()
+  console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
-  var lowercase = "I can't hear you!"
-}
-function sayHiToGrandma(string) {
-  return string.toUpperCase()
-  var uppercase = "YES INDEED!"
-}
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
