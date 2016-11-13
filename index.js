d0a6154e68b8649bc6675ea40806c0667b03fb98
Olufunsho Olagundoye <funsholag@gmail.com>
2016-11-13 00:28:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+};
+
+function whisper(string) {
+  return string.toLowerCase()
+};
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+};
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+};
+
+var lowercase = "hello"
+string.toLowerCase() === lowercase
+
+function sayHiToGrandma(lowercase) {
+    return "I can't hear you!"
+
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase
+
+function sayHiToGrandma(uppercase) {
+  return "YES INDEED!"
+};
+
+var ILoveYou = "I love you, Grandma."
+function sayHiToGrandma(ILoveYou) {
+  return "I love you, too."
+};
