025c007c1b1f71d920554e161741475b6d157559
Andrea Grygo <andreagrygo@gmail.com>
2016-11-21 04:30:49 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase === string){
-    return "'I can't hear you'";
+  if (string.toLowerCase() === string){
+    return "I can\'t hear you";
   }
-  if (string.toUpperCase === string){
+  if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
   if (string === "I love you, Grandma"){
