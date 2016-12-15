7460f3bd5c80f18ec9b483a4fd71365d39bf6a3e
Peter  Noel <petdnoel@gmail.com>
2016-12-15 14:48:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log().toUpperCase()
+}
+
+function sayHiToGrandma("I love you, Grandma.") {
+  return ("I can't hear you!")
+}
