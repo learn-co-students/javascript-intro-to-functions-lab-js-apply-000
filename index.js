6567c5569072e9e1df5cea383c1f008d17c7b401
Renee Baptiste <renbaptiste@gmail.com>
2017-01-03 23:43:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout (string){
+  return string.toUpperCase()
+}
+function whisper (string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  return string.toUpperCase()
+};
+function logWhisper(string){
+  return string.toLowerCase
+};
+function sayHiToGrandma (string){
+  return string.toUpperCase
+};
+function sayHiToGrandma (string){
+  return ("YES INDEED") string.toUpperCase
+};
+function sayHiToGrandma (string){
+  return "I love you, Grandma"
+};
