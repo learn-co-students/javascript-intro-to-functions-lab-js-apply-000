638b219635301cef4025f764be528e01aa299483
Scott Atkins <satkins16@gmail.com>
2017-01-03 15:19:10 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function logWhisper(string) {
   console.log("hello");
 }
 function sayHiToGrandma(string) {
-  if (string="hello") {return "I can't hear you!"}
-else if (string="HELLO") {return "YES INDEED!"}
+  if (string=="hello") {return "I can't hear you!"}
+else if (string=="HELLO") {return "YES INDEED!"}
+else if (string=="I love you, Grandma.") {return "I love you, too."}
 }
