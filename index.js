10691c6d983050a444157c862bfc15a13aa95195
Mehmetcan Budak <mehmetcanbudak@gmail.com>
2017-01-09 03:47:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+	return 'HELLO'
+string.toUpperCase()
+  }
+
+function whisper(string) {
+  	return 'hello'
+string.toLowerCase()
+  }
+
+
+function logWhisper(string) {
+	console.log('hello')
+string.toLowerCase()
+  }
+
+function logShout(string) {
+  console.log('HELLO')
+string.toUpperCase()
+  }
+
+  function sayHiToGrandma(string){
+    if (string === string.toLowerCase()) {
+      return "I can\'t hear you!"
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+    }else if (string === "I love you, Grandma.") {
+      return "I love you, too."
+    }
+
+  }
