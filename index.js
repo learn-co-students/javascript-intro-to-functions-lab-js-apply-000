3e8902cb38f7e284f62e71401fd92e406eaf06d6
Abraham  Kwak <kwak.abe@gmail.com>
2017-01-23 16:32:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-function shout(string) {
+var shout = string => {
   return string.toUpperCase();
 }
 
-function whisper(string) {
+// function shout(string) {
+//   return string.toUpperCase();
+// }
+
+var whisper = string => {
   return string.toLowerCase();
 }
 
-function logShout(string) {
+// function whisper(string) {
+//   return string.toLowerCase();
+// }
+
+var logShout = string => {
   console.log(string.toUpperCase());
 }
 
-function logWhisper(string) {
+// function logShout(string) {
+//   console.log(string.toUpperCase());
+// }
+
+var logWhisper = string => {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string) {
-  if(string === string.toLowerCase()) {
+// function logWhisper(string) {
+//   console.log(string.toLowerCase());
+// }
+
+var sayHiToGrandma = string => {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if(string === string.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  } else if(string === "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
+
+// function sayHiToGrandma(string) {
+//   if (string === string.toLowerCase()) {
+//     return "I can't hear you!";
+//   } else if (string === string.toUpperCase()) {
+//     return "YES INDEED!";
+//   } else if (string === "I love you, Grandma.") {
+//     return "I love you, too.";
+//   }
+// }
