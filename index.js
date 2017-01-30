1abb438fd2447d9cce92d68a6f9e74dfab387b28
Eduardo Bermeo <eduber6637@gmail.com>
2017-01-30 04:26:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+  console.log()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+  console.log()
+}
+function logShout(string) {
+  console.toUpperCase()
+}
