3c147c8046248beb2da2df2f9542e44a8a86d75f
RIKIT PATEL <patelrikit@gmail.com>
2017-02-24 20:01:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function sayHiToGrandma(st) {
+  return 'I can\'t hear you!'
+}
+function logShout('HELLO') {
+  return 'YES INDEED!'
+}
