b5427cb71fa9a5656347465eaefb96490dade5ae
javed islam <islam.javed@gmail.com>
2017-03-06 01:14:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  string === string.toLowerCase()
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(string){
+  string === string.toLowerCase()
+  return "I can\'t hear you!"
+}
+function sayHiToGrandma(string){
+  string === "I love you, Grandma."
+  return "I love you, too."
+}
