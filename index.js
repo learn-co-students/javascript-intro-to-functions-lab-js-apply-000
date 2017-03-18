3c1beb7900b582d4c6986728bc2e77b4023f3126
Arielle Miller <ariellem104@gmail.com>
2017-03-18 14:27:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logWhisper(string){
+  console.log(string).toUpperCase()
+}
+function logShout(string){console.log(string)
+  return (string).toUpperCase()
+}
