5cfc4ffba6677e63a33fae0debc3977598cdd2ab
Arielle Miller <ariellem104@gmail.com>
2017-03-19 18:43:00 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ function whisper(string){
 function logWhisper(string){
   console.log(string).toUpperCase()
 }
-function logShout(string){console.log(string)
-  return (string).toUpperCase()
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if(string===string.toLowerCase()){
+    return("I can't hear you!")
+  }
+  if(string===string.toUpperCase()){
+    return("YES INDEED!")
+  }
+  if(string==="I love you, Grandma."){
+    return("I love you, too.")
+  }
 }
--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
-  }
+  })
 })
 
 describe('whisper(string)', function() {
