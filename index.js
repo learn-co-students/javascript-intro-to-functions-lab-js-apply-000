046a2961cb0d313541ccc340468ebb313d013978
David Skaggs <davidtskaggs@gmail.com>
2017-04-03 18:22:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string) {
+  var upperCase = "HELLO";
+  var lowerCase = "hello";
+  if(string === lowerCase) {
+    return "I can't hear you!";
+  } else if(string === upperCase) {
+    return "YES INDEED!";
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  return;
+}
