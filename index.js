4e65032274fd6ebfc3f5dcd0735c0e6f7b2bc8b5
Jennifer Wilson <jennwilson55@gmail.com>
2017-04-11 02:24:36 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)  {
     return ("I can't hear you!")
   } else if (string.toUpperCase() === string) {
-      return ("YES INDEED!") 
-  } else {
-    if (string === "I love you, Grandma.")
-      return ("I love you, too.")
+    return ("YES INDEED!") 
+  } else if (string === "I love you, Grandma.") {
+    return ("I love you, too.") 
   }
 }
 
