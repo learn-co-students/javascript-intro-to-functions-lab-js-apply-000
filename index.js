059cf1ad82ef9087c6f0c4d04fe7364968d49d8a
Kaushal Patel <kaushalpatel778@gmail.com>
2017-04-13 16:15:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,60 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string)  {
+
+  console.log(string.toUpperCase())
+  }
+function logWhisper(string) {
+    console.log (string.toLowerCase())
+  }
+//*******************
+
+function sayHiToGrandma(string){
+var str = "hello"
+
+    if(str.toLowerCase()==str)
+      return "I can\'t hear you!"
+
+    else
+       return "YES INDEED!"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*function sayHiToGrandma(string){
+       var uppercase = "HELLO!"
+           uppercase.toUpperCase() === uppercase // true
+           return "I can\'t hear you!"
+
+          var lowercase = 'hello!'
+           lowercase.toLowerCase() === lowercase // true
+           return "YES INDEED!"
+
+        var mixedCase = 'Hi there!'
+          mixedCase.toLowerCase() === mixedCase // false
+           return "I love you, too."
+
+         mixedCase.toUpperCase() === mixedCase // false
+        return "I love you, too."
+
+
+
+  }*/
