e82a56858ebc219d81ab974f86b7c92f1b6aca44
Kaushal Patel <kaushalpatel778@gmail.com>
2017-04-13 16:22:56 +0000

    Done.

--- index.js
+++ index.js
@@ -6,30 +6,26 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string)  {
-
+function logShout(string) {
   console.log(string.toUpperCase())
-  }
+}
+
 function logWhisper(string) {
-    console.log (string.toLowerCase())
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(str) {
+  if (str.toLowerCase() === str) {
+    return "I can't hear you!"
   }
 
- function sayHiToGrandma(string){
-       var uppercase = "HELLO!"
-           uppercase.toUpperCase() === uppercase // true
-           return "I can\'t hear you!"
-
-          var lowercase = 'hello!'
-           lowercase.toLowerCase() === lowercase // true
-           return "YES INDEED!"
-
-        var mixedCase = "I love you, Grandma."
-          mixedCase.toLowerCase() === mixedCase // false
-           return "I love you, too."
-
-         mixedCase.toUpperCase() === mixedCase // false
-        return "I love you, too."
-
-
-
+  if (str.toUpperCase() === str) {
+    return "YES INDEED!"
   }
+
+  if (str === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  
+}
