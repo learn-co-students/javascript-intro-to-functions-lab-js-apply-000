5fd835521631944fe50297feb4f55a445cf57cf4
Burcu Noyan <bnoyan90@gmail.com>
2017-04-17 01:44:11 +0000

    Done.

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  return console.log(string.toUpperCase());
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  return console.log(string.toLowerCase());
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
