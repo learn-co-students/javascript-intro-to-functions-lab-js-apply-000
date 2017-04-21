32ddbc5a31ea8ca6765b4727e518d6d276c0ca5f
Brian Hodson <bhodson87@gmail.com>
2017-04-21 16:54:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+  return string.toUpperCase()
+  shout('hello')
+}
+function whisper(string){
+  return string.toLowerCase()
+  whisper("HELLO")
+}
+function logShout(string){
+  string = string.toUpperCase()
+  console.log(string)
+}
+function logWhisper(string){
+    string = string.toLowerCase()
+    console.log(string)
+}
+function sayHiToGrandma(string){
+    if (string === string.toLowerCase()){
+        return "I can't hear you!"
+      }
+}
+function sayHiToGrandma(string){
+    if (string === string.toLowerCase()){
+        return 'I can\'t hear you!'
+      } else if (string === string.toUpperCase()) {
+        return 'YES INDEED!'
+      }else if (string === "I love you, Grandma.") {
+        return 'I love you, too.'
+      }
+}
