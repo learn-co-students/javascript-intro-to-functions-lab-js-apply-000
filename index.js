8ac72339e57a3ece722ecb6010f5573a7df6481c
Alexander Lem <lem.alexander@gmail.com>
2017-05-03 02:58:53 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log string.toLowerCase
+  console.log(shout(string))
 }
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if (string === whisper(string)) {
+    return "I can't hear you!"
+  } else if (string === shout(string)) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  }
