ab7188f366c7fd983bb1e89391944734004445f5
Logan Emerson <logemerson@gmail.com>
2017-06-08 19:00:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 function shout(string){
   return string.toUpperCase();
-}
+};
 
 function whisper(string){
   return string.toLowerCase();
 };
 
 function logShout(string){
-  console.log(string.toUpperCase());
+  //console.log(string.toUpperCase());
+  console.log(shout(string));
 };
 
 function logWhisper(string){
-  console.log(string.toLowerCase());
+  //console.log(string.toLowerCase());
+  console.log(whisper(string));
 };
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase()){
+  if (string === whisper(string)){
     return "I can't hear you!";
-  } else if (string === string.toUpperCase()){
+  } else if (string === shout(string)){
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.");{
     return "I love you, too.";
