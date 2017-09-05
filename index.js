0d6baaff12dfe8717eae32b093532a5e48b43a61
Adiel Wilson <adielwilson@gmail.com>
2017-09-05 01:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper(string) {
 };
 function sayHiToGrandma(string) {
     if(string === string.toLowerCase()) return "I can't hear you!";
-    if(string === string.toUpperCase()) return "YES INDEED";
+    if(string === string.toUpperCase()) return "YES INDEED!";
     if(string === 'I love you, Grandma') return "I love you, too.";
 };
