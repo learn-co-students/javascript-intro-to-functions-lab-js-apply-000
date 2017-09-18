718d71e816cdca110792cd68c66ee4faa219e0ff
James Everett <jre0520@gmail.com>
2017-09-18 15:38:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function sayHiToGrandma(string) {
+   if(string.toLowerCase() === string) {
+     return "I can't hear you!"
+   }
+   else if (string.toUpperCase() === string) {
+     return "YES INDEED!"
+   }
+  else if("I love you, Grandma." === string) {
+     return "I love you, too."
+   }
+ }
