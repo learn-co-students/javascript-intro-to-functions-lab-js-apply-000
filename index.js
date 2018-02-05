826a037afc9f7a85d024eaa1f28701353605033c
Sophia Song <sophia.song@tufts.edu>
2018-02-05 21:16:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function shout(string) {return string.toUpperCase()
 }
-var lowerCase= "hello!"
-}
-function whisper(string) {return string.toLowerCase()
-}
-var lowerCase= "hello!"
-function sayHiToGrandma(string) {return string.toLowerCase(false)
-false.return "I can't hear you!"
-}
+function whisper(string) {return string.toLowerCase()}
+function logWhisper(string) {console.log; string.toLowerCase()}
 var logShout= "HELLO!"
-function logShout(string) {console.log string.toUpperCase()
+function logShout(string) {console.log; string.toUpperCase()}
+var lowerCase= "hello!"
+var lowerCase= "hello!"
+
+function sayHiToGrandma(string) {return string.toLowerCase() === string
 }
+{if (string.toLowerCase() === string)  { 
+  return "I can't hear you!";
+  }}
+{if (string === "I love you, Grandma.") {
+  return "I love you, too";
+  
+}}
+return "Are you eating enough?"
+{if (string.toUpperCase() === string) {
+  return  "YES INDEED!";
+}}
