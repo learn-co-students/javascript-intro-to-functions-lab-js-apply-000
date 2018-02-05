8c9c00eb2842c6b15c4d7306328b567e339ca2bc
Sophia Song <sophia.song@tufts.edu>
2018-02-05 20:10:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {return string.toUpperCase()
+}
+var lowerCase= "hello!"
+}
+function whisper(string) {return string.toLowerCase()
+}
+var lowerCase= "hello!"
+function sayHiToGrandma(string) {return string.toLowerCase(false)
+false.return "I can't hear you!"
+}
+var logShout= "HELLO!"
+function logShout(string) {console.log string.toUpperCase()
+}
