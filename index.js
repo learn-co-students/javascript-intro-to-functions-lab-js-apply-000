607a685e120b0c878d6a726130d420dbeb3d1985
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-02-25 22:28:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  var whatWasSaid = string
+  var whatWasSaid = string;
   if (whatWasSaid.toLowerCase() === whatWasSaid) {
     return 'I can\'t hear you!';
   }
   else if (whatWasSaid.toUpperCase() === whatWasSaid) {
-    return 'YES INDEED!'
+    return 'YES INDEED!';
   }
   else if (whatWasSaid === "I love you, Grandma.") {
-    return "I love you, too."
+    return "I love you, too.";
   }
 }
