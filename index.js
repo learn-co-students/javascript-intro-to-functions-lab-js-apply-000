47f4bc56357e4fb5067c6dcdf73dd8c82c6c5918
snkoski <shawnnkoski@gmail.com>
2018-03-01 23:07:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,9 @@ function sayHiToGrandma(string)
   if (string === string.toLowerCase) {
     return "I can't hear you!";
   }
+  if (string === string.toUpperCase) 
+  {
+    return "YES INDEED!";
+  }
   
 }
