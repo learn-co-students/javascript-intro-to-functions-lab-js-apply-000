bc7f8e2674cb7a210d3c7c22d28f92833d22fae6
snkoski <shawnnkoski@gmail.com>
2018-03-01 23:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ function logWhisper(string)
 function sayHiToGrandma(string) 
 {
   if (string === string.toLowerCase) {
-    return "I can't hear you!";
+    return 'I can\'t hear you!';
   }
   if (string === string.toUpperCase) 
   {
-    return "YES INDEED!";
+    return 'YES INDEED!';
   }
   if (string === "I love you, Grandma.") 
   {
