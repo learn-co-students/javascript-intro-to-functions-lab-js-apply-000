3976328a5288d01227527761a4d6061422d57baf
snkoski <shawnnkoski@gmail.com>
2018-03-01 23:11:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function sayHiToGrandma(string)
   }
   if (string === string.toUpperCase) 
   {
-    return 'YES INDEED!';
+    return "YES INDEED!";
   }
   if (string === "I love you, Grandma.") 
   {
