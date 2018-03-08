a0dd65b8858463d8bb5111e9fda7f5aa21ec0472
Daniel Shimon <danielzshimon@gmail.com>
2018-03-08 12:08:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function logWhisper(string){
 function sayHiToGrandma(string){
  if(string === "I love you, Grandma."){
    return "I love you, too.";
- } else if(string === string.toUpperCase() ){
+  } else if(string === string.toUpperCase() ){
    return "YES INDEED!";
- } else{
+  } else{
    return "I can't hear you!";
- }
+  }
  
 }
 
