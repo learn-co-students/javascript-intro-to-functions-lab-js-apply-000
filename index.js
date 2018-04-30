c54d4986788e5f64d6b608d9c20623287231127d
Allegra <aukseallegra@gmail.com>
2018-04-30 14:16:07 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string){
   if (string === string.toUpperCase()){
     return "YES INDEED!"
   }
-  if (string === "I love you, Grandma"){
+  if (string === "I love you, Grandma."){
     return "I love you, too."
   }
   
