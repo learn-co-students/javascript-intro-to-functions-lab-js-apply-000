fc7a7e5ff7f49edbe8b2e777dd6bc7bfe258f2cb
Yan Wang <wangyan6783@gmail.com>
2018-05-09 21:30:50 +0000

    Done.

--- index.js
+++ index.js
@@ -17,17 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === 'I love you, Grandma.') {
+  } else if (string === 'I love you, Grandma.') {
     return 'I love you, too.';
   }
 }
