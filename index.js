28d8e133fca074884e726998fe37b2d8eae9a77c
flatirongithub <38525422+flatirongithub@users.noreply.github.com>
2018-05-15 16:51:28 +0100

    Update index.js
    Added semicolons after realising they were missing?
    
    Why did the JS throw an error without them in the Sandbox? (also does not throw a error when run in console...so when would it throw an error?

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
-  return
+  console.log(string.toUpperCase());
+  return;
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
-  return
+  console.log(string.toLowerCase());
+  return;
 }
 
 function sayHiToGrandma(string) {
     if (string.toLowerCase() === string) {
-      return "I can\'t hear you!"
+      return "I can\'t hear you!";
     }
    
     if (string.toUpperCase() === string) {
-      return "YES INDEED!"
+      return "YES INDEED!";
     }
 
   if ("I love you, Grandma." === string) {
-    return "I love you, too."
+    return "I love you, too.";
   }
-  return
+  return;
 }
