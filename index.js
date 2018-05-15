a45ce93f2f43cd364dae05f183fc1d826bf92081
N <nirav2000+flatiron@gmail.com>
2018-05-15 15:31:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+  return
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+  return
+}
+
+function sayHiToGrandma(string) {
+    if (string.toLowerCase() === string) {
+      return "I can\'t hear you!"
+    }
+   
+    if (string.toUpperCase() === string) {
+      return "YES INDEED!"
+    }
+
+  if ("I love you, Grandma." === string) {
+    return "I love you, too."
+  }
+  return
+}
