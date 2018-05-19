0297e63b3e8a8ee40fa27bd5043bca51c021dbea
Jessica Yoo <jessmee1234@gmail.com>
2018-05-19 03:02:44 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
   } else if (string === string.toUpperCase()) {
-    return 'YES INDEED'
+    return 'YES INDEED!'
   } else if (string === 'I love you, Grandma.') {
     return 'I love you, too.' 
 }
