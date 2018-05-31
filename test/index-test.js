b9cae8cbb8680ea45f9a76db56c686a68a329617
ssingle2002@gmail.com <ssingle2002@gmail.com>
2018-05-31 23:20:14 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
 
-describe('shout(string)', function() {
+describe('hello !(string)', toUppercase() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
