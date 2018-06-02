07546704e6b2afa9694c79566e3e1d47f6ad6fa4
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-02 13:49:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
- return console.log(string.toUpperCase())
+ return console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  return console.log(string.toLowerCase())
+  return console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
   if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return "I love you, too.";
   }
 }
