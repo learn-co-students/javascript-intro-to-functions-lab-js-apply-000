ec74b03faae835c20ba3d8fdd7c352fd2910a332
nadine ebrani <nadinedoshkeh3@gmail.com>
2018-06-04 00:13:48 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string){
   }else if (string == string.toUpperCase()){
     return "YES INDEED!";
   }else if(string == "I love you, Grandma."){
-    return "I love you, too."
+    return "I love you, too.";
   }
 }
 
