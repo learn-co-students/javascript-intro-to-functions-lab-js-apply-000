89904f7a60f218cada496c5b80d44400d057f33e
kidtango <scott.q.tang@gmail.com>
2018-06-04 16:13:01 +0000

    Done.

--- index.js
+++ index.js
@@ -17,19 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
     if(string === string.toLowerCase()) {
       return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "I love you, Grandma.") {
+        return "I love you, too.";
     }
   }
-
-function sayHiToGrandma(string) {
-  if(string === string.toUpperCase()) {
-    return "YES INDEED!";
-  }
-}
-
-function sayHiToGrandma(string) {
-    const love = "I love you, Grandma.";
-    if(string === love) {
-      return "I love you, too.";
-    }
-  }
-  
