1ee3857098b81aa04741d7dd20c00e4240dc34bd
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-06-14 18:56:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const logShout = string => console.log(string.toUpperCase());
 const logWhisper = string => console.log(string.toLowerCase());
 
 const sayHiToGrandma = string => {
+  // If this function had a default behavior, I might opt to use ternary
   if (string === 'I love you, Grandma.') return 'I love you, too.';
   if (string === string.toLowerCase()) return 'I can\'t hear you!';
   if (string === string.toUpperCase()) return 'YES INDEED!';
