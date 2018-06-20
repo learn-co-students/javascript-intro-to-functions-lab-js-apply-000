13c4bd8d91c6c7d5e3b928bb91d4aaf0634be2a7
EdPutans Putans <edputans@gmail.com>
2018-06-20 14:19:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+string = "Hello!"
+
+console.log(string)
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()===string) {
+    return("I can't hear you!")}
+  if (string==='I love you, Grandma.') {
+    return('I love you, too.')}
+  if (string.toUpperCase()===string) {
+    return("YES INDEED!")}
+  else{
+    return('SAY WHAT, HONEY?')
+  }
+}
+console.log(sayHiToGrandma)
