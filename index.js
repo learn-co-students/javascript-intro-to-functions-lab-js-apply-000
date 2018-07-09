7c729c61ed97fd3508fa8a16d4f2e9f1f12e6038
Tiffany Choong <t.choong@yahoo.com>
2018-07-09 22:19:51 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper('hello'))
 }
+
+
+  
+
+function sayHitoGrandma(string) {
+  
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  var mixedCase = "I love you, Grandma."
+  
+  uppercase.toUpperCase() === uppercase // true
+  lowercase.toLowerCase() === lowercase // true
+  mixedCase.toLowerCase() === mixedCase // false
+  mixedCase.toUpperCase() === mixedCase // false
+ 
+ 
+}
