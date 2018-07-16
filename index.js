f8ea12b0506da133c965762285b01bfac5763dd4
Benjamin Lee <nycxbl@hotmail.com>
2018-07-16 02:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-function shout(string)
-{
+function shout(string){
   return string.toUpperCase();
 }
-function whisper(string)
-{
+
+function whisper(string) {
   return string.toLowerCase();
 }
-function sayHiToGrandma(string)
-{
-  if (string === "I love you, Grandma.") {
-     return "I love you, too.";
-  }
-  else if(string.toLowerCase() === string) {
-   return "I can\'t hear you!";
-  }
-  else if (string.toUpperCase() === string){
-     return  "YES INDEED!";
-  } 
-}
-function logShout(string)
-{
+
+function logShout(string){
   console.log(string.toUpperCase());
 }
-function logWhisper(string)
-{
+
+function logWhisper(string){
   console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  
+  if(string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+  return "Are you eating enough?"; // not tested
 }
