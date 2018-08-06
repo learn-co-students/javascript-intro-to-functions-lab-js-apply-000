79c6a18f01768229aa9e97626a58d756ce96db56
Kyle George <k.george21@outlook.com>
2018-08-06 22:49:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+const spy = ('HELLO');
+console.log(spy);
+}
+function logWhisper(string) {
+  const spy = ('hello');
+  console.log(spy);
+}
+function sayHiToGrandma(string) {
+  if (string==string.toLowerCase()) {
+    return ("I can't hear you!")
+  }
+  if (string==string.toUpperCase()){
+    return ("YES INDEED!")
+  }
+  if (string=='I love you, Grandma.'){
+    return ("I love you, too.")
+}
+}
