fc0f9b3f01158d5756aa3220c14551fe7c7f2912
sydneygrant <sgrant@nyu.edu>
2018-08-15 14:26:03 +0000

    Done.

--- index.js
+++ index.js
@@ -15,21 +15,12 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  // var lowercase = string;
   if (string === string.toLowerCase()){
     return "I can't hear you!";
   }
-}
-
-function sayHiToGrandma(string){
-  // var uppercase = string;
-  if (string === 'HELLO'){
-    return "YES INDEED!";
-  }
-}
-
-function sayHiToGrandma(string){
-  if (string === "I love you, Grandma."){
+  else if (string == string.toUpperCase()){
+    return 'YES INDEED!';
+  } else if (string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
