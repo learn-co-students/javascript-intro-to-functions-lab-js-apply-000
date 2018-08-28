36db93ecd80f5ccace0bc54e174134f71f7b6ba5
Sarah Pai <pai.sarah89@gmail.com>
2018-08-28 13:21:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-function shout(string) {
+function shout(string){
   return string.toUpperCase()
-  }
-  
-  function whisper(string) {
-    return string.toLowerCase()
-  }
-  
-  function logShout(hello){
-    console.log(hello.toUpperCase())
-  }
-  
-  function logWhisper(HELLO){
-  console.log(HELLO.toLowerCase())
-  }
-  
+}
 
-  function sayHiToGrandma(string){
-  if(string == "I love you, Grandma."){
-  return 'I love you, too.'
-  } else if (string == string.toUpperCase()){
-    return 'YES INDEED!'
-  } else (string == string.toLowerCase())
-    return 'I can\'t hear you!'
-  }
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log (string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log (string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+if (string === "I love you, Grandma."){
+return "I love you, too.";
+} else if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+} else if (string === string.toLowerCase()) { 
+  return "I can't hear you!"
+}
+  
+}
