430e07fd3a611dd69393b4cd564c44d7ffceac18
baikadamova.alua <baikadamova.alua@gmail.com>
2018-09-15 08:38:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  
+  return console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  
+  return console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  var a=string
+if (a==='HELLO'){
+  return ("YES INDEED!")
+}
+if (a==='hello'){
+  return ("I can't hear you!")
+}
+if(a==="I love you, Grandma."){
+  return ("I love you, too.")
+}
+return (a.toUpperCase)
+
+}
