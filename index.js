cceae94d1fe5651e6344516ee08f4d54922530e5
mjgreene2010 <mjgreene2010@gmail.com>
2018-09-24 13:01:12 +0000

    Done.

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   // if string is lowercase
     //then return "I can't hear you"
-  if (string == string.toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return  "I can't hear you!"
-  } else if (string == string.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
-  } else if (string == "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   } else { 
     return "do not return"
@@ -33,4 +33,5 @@ function sayHiToGrandma(string){
 //     return "YES INDEED!"
 //   }
 // }
+
 
