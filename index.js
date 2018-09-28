db3ae3144bef3585bf79e934e4e706aae5ba2c49
Cris Hanks <crishanks12@gmail.com>
2018-09-28 17:47:31 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if (string.toUpperCase() === string) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+  } else if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
   }
 }
