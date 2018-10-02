4aae2b4367906dc71c3aa41f426331997041125b
Ariel Hicks <hicks.ariel@gmail.com>
2018-10-02 19:02:41 +0000

    Done.

--- index.js
+++ index.js
@@ -11,15 +11,11 @@ function logWhisper(string) {
   console.log (string.toLowerCase('I can\'t hear you!'))
 }
 function sayHiToGrandma(string) {
-	if (string === string.toUpperCase()) {
+	if (string.toUpperCase() === string) {
   		return "YES INDEED!"
       }
 }
 function sayHiToGrandma(string) {
   if (string.toUpperCase('I love you, Grandma.'))
   return "I love you, too."
-}
-function sayHiToGrandma(string) {
-  if (string.toLowerCase())
-  return "I can\'t hear you!"
 }
