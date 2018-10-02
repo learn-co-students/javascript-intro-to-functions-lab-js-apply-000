7ee61650026740b35e840673d4de95e06cc4e189
Cory Adams <coadams9@gmail.com>
2018-10-02 23:09:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if string.toLowerCase() === lowercase 
+    return string("I can't hear you!")
+  elsif string.toUpperCase() === toUpperCase
+    return string("YES INDEED!")
+  else string.mixedCase() === "I love you, Grandma."
+    return string("I love you, too.")
+  end
+}
