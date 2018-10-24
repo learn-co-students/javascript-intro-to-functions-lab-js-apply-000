87f47cd5435884f4e4a1cba1ce2fe3825b7d893f
Alexander Hare <alexanderhare14@gmail.com>
2018-10-24 01:00:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function shout(string){
+function shout(string) {
   return string.toUpperCase()
 }
 function whisper(string) {
@@ -11,8 +11,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()){
-    return "I can't hear you!"
+  if ( string === string.toLowerCase() ) {
+    return ("I can't hear you!")
   }
+  else if ( string === string.toUpperCase() ) {
+    return ("YES INDEED!")
   }
+  else {
+    return ("I love you, too.")
+}
 }
