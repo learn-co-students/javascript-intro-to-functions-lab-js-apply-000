16a714e2eae4bcb238b0e29cc664052acf34f4a3
Chine Anikwe <c.anikwe@gmail.com>
2018-10-30 23:22:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('Hello'.toUpperCase())
+}
+function logWhisper(string) {
+  console.log('Hello'.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if(string == 'hello') {
+    return "I can\'t hear you!"
+  }
+  else if(string == 'HELLO') {
+    return "YES INDEED!"
+  }
+  else if(string == 'I love you, Grandma.') {
+    return "I love you, too."
+  }
+}
+sayHiToGrandma('Hello'.toLowerCase)
+sayHiToGrandma('Hello'.toUpperCase)
+sayHiToGrandma('I love you, Grandma.')
