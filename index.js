f92b017d9ce82206d139afb1245d5c2eb96f1ca5
John Crespi Oncher <johnoncher@gmail.com>
2018-11-22 15:41:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function shout(String){
-  return String.toUpperCase()
+  return String.toUpperCase();
 }
 function whisper(String){
-  return String.toLowerCase()
+  return String.toLowerCase();
 }
 function logShout(String){
-  console.log(shout(String))
+  console.log(shout(String));
 }
 function logWhisper(String){
-  console.log(whisper(String))
+  console.log(whisper(String));
 }
 function sayHiToGrandma(String){
-  if(String === whisper(String){
-    return console.log("I can't hear you!")
-  } )
+  if (String === whisper(String))
+  {return "I can't hear you!"}
+  
+  else if (String === "I love you, Grandma.")
+ {
+   return "I love you, too."
+ } 
+ else if (String === shout(String));
+ { return "YES INDEED!"}
+ 
+
 }
