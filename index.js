1eaa1f4b6506daef8b29351f12ccc0e7fc486f68
Stefani Hatton <stefani.hatton@gmail.com>
2018-11-28 23:11:56 +0000

    Done.

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ function sayHiToGrandma(string) {
   }else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }else if (string === "I love you, Grandma.") {
-    return "I love you,too."
+    return "I love you, too."
   }
+}   
