085618863ec3afa6c814906cacc6b3ad2f0feb17
Robert Han <robmj.han@gmail.com>
2018-12-03 14:30:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  if (string===string.toLowerCase()) 
-return ("I can't hear you!") 
-}
+if (string===string.toLowerCase()) {
+  return ("I can't hear you!"); }
+ else {
+return ("YES INDEED!"); }}
 
 function sayHiToGrandma(string) {
-  if (string===string.toUpperCase())
-return ("YES INDEED!")
+  if (string===("I love you, Grandma.")) {
+  return ("I love you, too."); }
 }
 
-function sayHiToGrandma(string) {
-  if (string===string("I love you, Grandma"))
+
+
