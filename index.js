7d89b4df6bdfcf9e1db8fcc16c01134b39e12fc9
Robert Han <robmj.han@gmail.com>
2018-12-04 07:00:40 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function sayHiToGrandma(string) {
     return ("I can't hear you!"); }
     else {
       return ("YES INDEED!"); }
+      
       if (string===("I love you, Grandma.")) {
   return ("I love you, too."); }
 }
