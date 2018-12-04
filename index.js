e647fd5cb53ae24b0620554267cfb49ca8cbabb8
Robert Han <robmj.han@gmail.com>
2018-12-04 06:28:41 +0000

    Done.

--- index.js
+++ index.js
@@ -14,16 +14,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string) {
-if (string===string.toLowerCase()) {
-  return ("I can't hear you!"); }
- else {
-return ("YES INDEED!"); }}
 
-function sayHiToGrandma(string) {
-  if (string===("I love you, Grandma.")) {
-  return ("I love you, too."); }
-}
+
 
 
 
