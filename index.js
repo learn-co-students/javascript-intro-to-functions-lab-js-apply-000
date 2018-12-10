22ae41e5d7591becb14f721728a2eecf06cd9c34
Paul Riverain <paulriverain@yahoo.com>
2018-12-10 17:44:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function shout(string){
   return string.toUpperCase()
 }
@@ -11,13 +12,12 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === string){
+  if(string.toLowerCase() === string){
     return "I can't hear you!"
   }
   if (string.toUpperCase() === string){
     return "YES INDEED!"
-  }
-  if (string === "I love you, Grandma."){
+  }else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
-}
+  }
