8c480e6d95004b7c76f1d5cc72917d6fb41d3f7d
authentic-actor-8801 <xaviernational@aol.com>
2019-01-11 22:07:12 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,18 +1,26 @@
 
 describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+  it('receives one argument and returns it in all caps',
+  function() 
+  {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
+describe('whisper(string)', function()
+{
+  it('receives one argument and returns it in all lowercase', 
+  function() 
+  {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
 
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
+describe('logShout(string)', function()
+{
+  it('calls console.log() its one argument in all caps', 
+  function() 
+  {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     logShout('hello')
@@ -24,7 +32,9 @@ describe('logShout(string)', function() {
 })
 
 describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
+  it('calls console.log() its one argument in all lowercase',
+  function() 
+  {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     logWhisper('HELLO')
