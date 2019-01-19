4e5ef6832d608148607b2a395a438ee60000ee1e
Karyna Dominguez <karinaminty@gmail.com>
2019-01-19 18:18:19 +0000

    Done.

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function logWhisper(string){
  console.log(string.toLowerCase())
 
 }
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+  
+}
