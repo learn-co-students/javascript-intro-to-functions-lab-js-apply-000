ac12361aad9fb7589239122907bcaa49fbbd07d5
Gioacchino Di Nardo <gio.amms@gmail.com>
2019-01-21 08:45:50 +0000

    Done.

--- index.js
+++ index.js
@@ -22,13 +22,12 @@ function logWhisper(string){
   }
 }
 
-//Or just a bunch of if statements...
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
   }else if (string === string.toUpperCase()) {
     return "YES INDEED!"
-  }else if (string === "I love you, Grandma.") {
+  }else if (string === 'I love you, Grandma.') {
     return "I love you, too."
   }
 }
