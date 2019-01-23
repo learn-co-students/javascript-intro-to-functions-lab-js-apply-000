44453a85b7ec584c6660d56a5b922344f476eb7a
Ella Taber <michaella.latkovic@gmail.com>
2019-01-23 05:33:28 +0000

    Done.

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function  sayHiToGrandma(string){
     return "I can't hear you!";
   }
   
-  if (string===string.toUpperCase()) {
+  else if (string===string.toUpperCase()) {
     return "YES INDEED!";
   }
   
-  if (string==="I love you, Grandma.") {
+  else if (string==="I love you, Grandma.") {
     return "I love you, too.";
   }
 }
