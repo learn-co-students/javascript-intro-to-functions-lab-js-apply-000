bbbae00cfef62239aab4423e8069ec1874fdddc5
Daniel Frankel <dfrankel@uchicago.edu>
2019-01-27 19:28:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var uppercase, lowercase, mixedCase;
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase // true
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase // true
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase // false
+mixedCase.toUpperCase() === mixedCase // false
 
 function shout(string) {
   return string.toUpperCase()
