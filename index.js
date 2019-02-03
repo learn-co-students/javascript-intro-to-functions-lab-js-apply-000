0d2c8de2e6051d1a1e3fd7b4d2eebc28146835ab
Anum Siddiqui <asiddiqui0692@gmail.com>
2019-02-03 00:04:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function shout('Hello!') {
-  return 'Hello!'.toUpperCase()
-}
+// function shout(string) {
+  // return string.toUpperCase()
+// }
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase ==== lowercase
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() ==== mixedCase
+
+mixedCase.toUpperCase() === mixedCase
