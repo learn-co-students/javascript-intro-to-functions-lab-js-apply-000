76e3087d1278b94291045fd7aa45b858528e4c48
Carlos Barrera <barrera9696@gmail.com>
2019-02-06 05:16:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string) {
+  return 'HELLO'
+string.toUpperCase()
+}
+function whisper(string) {
+  return 'hello'
+string.toLowerCase();
+}
+function logShout(string) {
+  console.log('HELLO');
+}
+function logWhisper(string) {
+  console.log('hello');
+}
+function sayHiToGrandma(string) {
+ if (string.toLowerCase() === string) {
+   return 'I can\'t hear you!';
+}
+ if (string.toUpperCase() === string) {
+   return 'YES INDEED!';
+}
+ if (string === 'I love you, Grandma.') {
+  return 'I love you, too.';
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+  
