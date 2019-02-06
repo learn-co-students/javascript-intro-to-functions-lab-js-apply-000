d1d5be0760d067ef940122b0ee8661944b97ce1b
Blake A Long <blake.a.long@gmail.com>
2019-02-06 03:39:19 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  }
-}
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
