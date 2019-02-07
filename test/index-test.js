71fdb3f0b8225e66689fb4ca1412839c483f3acd
compiled-writer-1970 <kevypark@gmail.com>
2019-02-07 16:47:45 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,50 +1,30 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
-})
+function shout(str) {
+  return str.toUpperCase();
+}
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
-  })
-})
+function whisper(str) {
+  return str.toLowerCase();
+}
 
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
+function logShout(str) {
+  console.log(str.toUpperCase());
+}
 
-    logShout('hello')
+function logWhisper(str) {
+  console.log(str.toLowerCase());
+}
 
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
-
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-})
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+function sayHiToGrandma(str) {
+var answer = '';
+  if(str === str.toLowerCase()) {
+    answer = 'I can\'t hear you!';
+  }
+  else if(str === str.toUpperCase()) {
+    answer = 'Yes Indeed!';
+  }
+  else if (str === 'I love you, Grandma.') {
+    answer = 'I love you, too.'
+  }
+  return answer;
+}
