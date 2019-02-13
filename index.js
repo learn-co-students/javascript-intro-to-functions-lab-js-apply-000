c9542abe0bef16a0c00bd87c1c3ade1a283241b7
Adam Brett Minkus <adam.minkus@gmail.com>
2019-02-13 00:42:52 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function whisper(string){
 function logShout(string){console.log('HELLO')}
 function logWhisper(string){console.log('hello')}
 function sayHiToGrandma(string){
-  return ('hello')
+  return string
 }
