667ac211dbff771c986cbf3ae33f8400cb2e2d1a
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-02-18 15:59:28 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout(string) {
   return console.log(string.toUpperCase())
 }
 
+function whipser(string) {
+  return string.toLowerCase()
+}
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!"
+  
+}
