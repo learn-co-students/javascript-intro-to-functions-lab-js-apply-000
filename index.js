982526eda2b6117e71e77b885ff549c398c06b93
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-02-23 15:09:12 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   } else if (string === shout(string)){
     return "YES INDEED!";
-  } else {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
