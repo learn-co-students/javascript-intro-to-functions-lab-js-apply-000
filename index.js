f4fcb5efea2effe6a02b61cbe89fbc6753d84dbb
Andrea Haveman <andhaveman@hotmail.com>
2019-02-23 23:23:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  const spy = 'HELLO';
+  console.log(spy);
+}
+function logWhisper(string) {
+  const spy = 'hello';
+  console.log(spy);
+}
+
+
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  return "I can\'t hear you!";
+}
+  if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+}
+  if (string === "I love you, Grandma.") {
+  return "I love you, too.";
+}
+}
+
