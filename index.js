db13c7fb88b65b67a0c82e01d6dd5f138dc201c7
Ronak Parekh <rparekh1996@gmail.com>
2019-03-04 14:35:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout (String){
+return String.toUpperCase();
+}
+function whisper (String){
+return String.toLowerCase();
+}
+function logShout(String){
+console.log(String.toUpperCase());
+}
+function logWhisper (String){
+console.log(String.toLowerCase());
+}
+function sayHiToGrandma(String){
+if (String == toLowerCase());
+return("I can't hear you");
+}
+if (String === String.toUpperCase());
+  return ("YES INDEED!");
+}
+else (String"I love you,grandma.");
+return (" I love you, too.");
+}
