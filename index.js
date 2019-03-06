6daa41919ff2e04a064eed7da5e857c9ea02a419
Blake A Long <blake.a.long@gmail.com>
2019-03-06 02:49:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-function shout(string) {
+var shout = (string) => {
   return string.toUpperCase();
-}
-function whisper(string) {
+};
+
+var whisper = (string) =>  {
   return string.toLowerCase();
-}
-function logShout(string) {
+};
+
+var logShout = (string) => {
   console.log(string.toUpperCase());
-}
-function logWhisper(string) {
+};
+
+var logWhisper = (string) => {
   console.log(string.toLowerCase());
-}
-function sayHiToGrandma(string) {
+};
+
+var sayHiToGrandma = (string) => {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
@@ -18,6 +22,7 @@ function sayHiToGrandma(string) {
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
-}
+};
+
 
 
