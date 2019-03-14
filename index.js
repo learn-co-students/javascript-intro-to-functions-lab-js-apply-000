ec73cc4f2f0a9523581ea315302f394d6d22fb1a
feven <feventsegay6@gmail.com>
2019-03-14 17:10:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(String) {
+  return String.toUpperCase()
+}
+function whisper(String){
+  return String.toLowerCase()
+}
+function logShout(String){
+  console.log(String)
+}
+function logWhisper(String){
+  console.log(String)
+}
+function sayHiToGrandma(string){
+  
+  return string
+}
