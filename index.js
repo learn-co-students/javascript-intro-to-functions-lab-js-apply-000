18a700b497f0be5bd1273fc1e457b77ddce3651a
Jake L <jakelovitz@gmail.com>
2019-03-20 01:02:02 +0000

    Done.

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout() {
-  console.log("HELLO")
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
 
-function logWhisper() {
-  console.log("hello")
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma (string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  
   if (string === "I love you, Grandma.")
    return "I love you, too."
 }
