7c175b08d36dd36d3265675959150c6f05b5e8e3
Luke Menard <lukemenard@gmail.com>
2019-03-26 02:48:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+var uppercase = "HELLO"
+//Defines the variable 'uppercase' and sets it equal to the string 'HELLO'
+
+var lowercase = "hello"
+//Defines the variable 'lowercase' and sets it equal to the string 'hello'
+
+function shout(string) {
+  return string.toUpperCase()
+}
+//Defines the function 'shout' that takes one argument, 'string'
+//Using the toUpperCase() function, 'shout' returns the entered string in all caps
+
+function whisper(string){
+  return string.toLowerCase()
+}
+//Defines the function 'whisper' that takes one argument, 'string'
+//Using the toLowerCase() function, 'whisper' returns the entered string in lower case
+
+function logShout() {
+ console.log("HELLO")
+}
+//Defines the function 'logShout' that takes no arguments
+//Prints the string "HELLO"
+
+function logWhisper() {
+ console.log("hello")
+}
+//Defines the function 'logWhisper' that takes no arguments
+//Prints the string "hello"
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return 'I can\'t hear you!'
+  }
+  else if (string === uppercase) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+//Defines the function 'sayHiToGrandma' that takes one argument, 'string'
+//If the entered string is relationally equal to the 'lowercase' variable ('hello'), the function returns the string 'I can't hear you!'
+//Otherwise, if the entered string is relationally equal to the 'uppercase' variable ('HELLO'), the function returns the string 'YES INDEED!'
+//If the entered string is relationally equal to the string 'I love you, Grandma', the function returns the string 'I love you too.'
