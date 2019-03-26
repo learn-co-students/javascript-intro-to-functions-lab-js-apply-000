ba4dc165a94795195493b86f49e198facdb2e169
Luke Menard <lukemenard@gmail.com>
2019-03-26 03:45:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-var uppercase = "HELLO"
+var uppercase = "HELLO";
 //Defines the global variable 'uppercase'
 //Assigns 'uppercase' the string 'HELLO'
 
-var lowercase = "hello"
+var lowercase = "hello";
 //Defines the variable 'lowercase'
 //Assigns 'lowercase' the string 'hello'
 
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 //Defines the function 'shout' that takes one argument, 'string'
 //Using the toUpperCase() function, 'shout' returns the entered string in all caps
 
 function whisper(string){
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 //Defines the function 'whisper' that takes one argument, 'string'
 //Using the toLowerCase() function, 'whisper' returns the entered string in lower case
 
 function logShout() {
- console.log("HELLO")
+ console.log("HELLO");
 }
 //Defines the function 'logShout' that takes no arguments
 //'logShout' prints the string "HELLO"
 
 function logWhisper() {
- console.log("hello")
+ console.log("hello");
 }
 //Defines the function 'logWhisper' that takes no arguments
 //logWhisper prints the string "hello"
 
 function sayHiToGrandma(string) {
   if (string === lowercase) {
-    return 'I can\'t hear you!'
+    return 'I can\'t hear you!';
   }
   else if (string === uppercase) {
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
   else if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return "I love you, too.";
   }
 }
 //Defines the function 'sayHiToGrandma' that takes one argument, 'string'
