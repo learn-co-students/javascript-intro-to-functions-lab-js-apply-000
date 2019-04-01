91afced732ae1b045e8844837dd99aea6e010419
Neal JIn Oh <no4259@gmail.com>
2019-04-01 00:12:51 +0000

    Done.

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   if (string === string.toUpperCase()) {
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
-  return "I love you, too."
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
+
