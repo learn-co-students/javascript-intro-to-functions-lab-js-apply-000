728a8e233e9261f238074f735c3c9d2341d2feab
James Boynton <james.boynton@gmail.com>
2019-04-10 23:06:35 +0000

    Done.

--- test/root.js
+++ test/root.js
@@ -28,3 +28,17 @@ before(function(done) {
     return done();
   });
 });
+
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
