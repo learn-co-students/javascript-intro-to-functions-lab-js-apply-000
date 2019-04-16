3e89c42207ee9fbe09c8dd537f48913aeab68061
Megan Adams <meganoliviawendyadams@gmail.com>
2019-04-16 05:46:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log("HELLO");
+}
+
+function logWhisper(string) {
+   console.log("hello");
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO";
+  var lowercase = "hello";
+  
+  
+  if ((string) === lowercase) 
+  return ("I can't hear you!");
+  
+  if ((string) === uppercase)
+  return ("YES INDEED!");
+  
+  if ((string) === ("I love you, Grandma."))
+  return ("I love you, too.")
+  }
+  
+  //It's so damn simple once it actually clicks, but until then you feel like you're seven years old trying to figure out how to a put a broken egg back together again.
+  
+  // Then when you do finally figure it out you feel like Bill Gates himself. Best feeling.
