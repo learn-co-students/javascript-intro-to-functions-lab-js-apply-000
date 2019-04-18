b5633349f031c0052e50ca820612ae63ee6d3cdf
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-04-18 15:21:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string){
+  return string.toUpperCase() 
+}
+function whisper(string){
+  return string.toLowerCase() 
+}
+function logShout(string){
+  console.log(string.toUpperCase()) 
+}
+function logWhisper(string){
+  console.log( string.toLowerCase()) 
+}
+function sayHiToGrandma(string){
+  return  "I can't hear you!" 
+}
+if ("i can\'t hear you!" === string.toLowerCase() ) {
+  return "YES INDEED!";
+}
+
+  
+  
