28d66f574257abf7fb32fe90245da789c9aad201
Felipe <felipeldn91@gmail.com>
2019-04-25 22:31:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log (string.toUpperCase())}
 function logWhisper(string) {console.log (string.toLowerCase())}
 
-function sayHiToGrandma(string) {if(string.toLowerCase()==string) return "I can't hear you!"
-else {if(string.toUpperCase()==string) return "YES INDEED!"
-else {if("I love you, Grandma"==string) return "I love you, too."
+function sayHiToGrandma(string) {
+  if(string.toLowerCase()==string) {
+    return "I can't hear you!";
+  }
+else if(string.toUpperCase()==string) {
+  return "YES INDEED!";
+}
+
+else if("I love you, Grandma."==string) {
+  return "I love you, too.";
+  
+}
   
 }
