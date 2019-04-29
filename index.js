4ee0197af89ae0118af4c605fc536bd475eae596
algebraic-animation-3644 <nathanchan1007@gmail.com>
2019-04-29 06:04:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(shout(string))
+}
+function logWhisper(string) {
+    console.log(whisper(string))
+}
+function sayHiToGrandma(string) {
+    if(string === lowercase) {
+        return "I can't hear you!"
+    elseif(string === "I love you, Grandma.")
+        return "I love you, too."
+    else
+        return "YES INDEED!"
+
+    }
+}
