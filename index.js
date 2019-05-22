e4a1fb296bada444a7ffa9eb915d851525938aee
Marco Calvieri <calvieri.marco@gmail.com>
2019-05-22 19:06:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+'Hello!'.toUpperCase()
+
+
+function whisper(string) {
+  return string.toLowerCase() 
+}
+'Hello!'.toLowerCase()
+
+
+function logShout(string) {
+  console.log("HELLO");
+  
+}
+"Hello!".toUpperCase()
+
+
+function logWhisper(string) {
+  console.log("hello");
+  
+}
+"HELLO!".toLowerCase()
+
+
+var uppercase = "HELLO!"
+var lowerCase = "hello!"
+var mixedCase = "i can't hear you!"
+
+
+function sayHiToGrandma(string){
+if (string.toLowerCase() === string)
+  return "I can't hear you!"
+
+if (string.toUpperCase() === string)
+  return "YES INDEED!"
+
+if ("I love you, Grandma." === string)
+  return "I love you, too."
+}
+
