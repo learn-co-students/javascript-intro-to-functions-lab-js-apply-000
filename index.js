992a48208bc57918bf575069491a73e473c3cfdd
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-05-26 12:57:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function logShout(string) {
-  console.log(string.toUpperCase)
+function whisper(string) {
+  return string.toLowerCase()
 }
 
+function shout(string) {
+  return string.toUpperCase()
+}
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  string.toUpperCase() === string
+  return "YES INDEED!"
+  string.toLowerCase() === string
+  return "I can't hear you!"
+  string === "I love you, Grandma."
+  return "I love you, too."
+}
