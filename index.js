ceb3c10b2bf4c29a04c41e3d01703b8b6e2771fc
Aibek Ozhorov <ozhorov@gmail.com>
2019-05-30 18:31:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function speak (whisper) {return whisper}
+function speak (shout) {return shout}
+function say (shout)  {return shout}
+'HI grandma!'.toUppercase()//HI
+function say (whisper) {return whisper} 
+function say (shout) {return shout} 
+'I love you Grandma'.toUppercase()//I love you Grandma
+
