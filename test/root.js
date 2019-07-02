c29bdfa98724104ac6a1620105d701c63dedd548
Robert Eggen <rjegrad@yahoo.com>
2019-07-02 17:58:49 +0000

    Done.

--- test/root.js
+++ test/root.js
@@ -13,18 +13,21 @@ before(function(done) {
 
   const html = path.resolve(__dirname, '..', 'index.html')
 
-  jsdom.env(html, [], {
-    src: babelResult.code,
-    virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-  }, (err, window) => {
-    if (err) {
-      return done(err);
-    }
 
-    Object.keys(window).forEach(key => {
-      global[key] = window[key];
-    });
 
-    return done();
-  });
-});
+function shout(string) {
+  return string.toUpperCase()
+}
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
