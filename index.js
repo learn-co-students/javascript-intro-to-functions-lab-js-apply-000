3fc5431c5a4421e74a493e32b89bd952e4207f9b
Rachel Fuerst <rach.fuerst@gmail.com>
2019-07-05 00:17:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  return console.log("HELLO")
+}
+
+function logWhisper(string){
+  return console.log("hello")
+}
+
+// var sayHiToGrandma;
+// if (string.toLowerCase() === string) {
+//   sayHiToGrandma = function(string) {
+//     return "I can\'t hear you!"
+//   }
+// }
+
+function sayHiToGrandma(string) {
+  var response;
+  if (string.toLowerCase() === string) {
+    response = "I can\'t hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    response = "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    response = "I love you, too."
+  }
+
+  return response
+}
+
+
+// function sayHiToGrandma(string) {
+//   var uppercase = 'YES INDEED!'
+//   string.toUpperCase() === string
+//   return uppercase
+//
+// }
