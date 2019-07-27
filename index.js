30521503a5c708b4f8db4f2a85b9b35fb2be6012
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-07-27 17:23:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout (string) {
+  return string. toUpperCase()
+  }
+function whisper (string) {
+  return string. toLowerCase()
+}
+function logShout (string) {
+  console.log(string. toUpperCase())
+}
+function logWhisper (string) {
+  console.log (string. toLowerCase())
+}
+function sayHiToGrandma(string) {
+  var lowercase = "I can't hear you!";
+  var uppercase = "YES INDEED!";
+  var mixedCase = "I love you, too.";
+  if (string. toLowerCase(string) ===string) {
+    return "I can't hear you!";
+  }
+  else if (string.toUpperCase (string) ===string) {
+    return "YES INDEED!";
+  }
+  else if ("I love you, Grandma." === string) {
+    return "I love you, too."
+  }
+}
