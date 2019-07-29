f54c3f8b973340d43b4d889b6e5381b6f8944305
Urfan Guliyev <urfan196@yahoo.com>
2019-07-29 01:08:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-function shout (string) {
-  return string.toUpperCase ()
+function shout(string) {
+  return string.toUpperCase()
 }
-shout ('hello')
+shout("hello")
 
-function whisper (string) {
-  return string.toLowerCase ()
+function whisper(string) {
+  return string.toLowerCase()
 }
-shout ("HELLO")
+whsiper("HELLO")
 
-function logShout(string) {
+function logShout(string){
   console.log(string.toUpperCase());
 }
 logShout("hello")
 
-function logWhisper(string){
+function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 logWhisper ("HELLO")
 
-function sayHiToGrandma (string){
-  if (string === string.toLowerCase()){
-    return "I can't hear you!"
-  } else if (string === string.toUpperCase ()) {
-    return "YES INDEED!"
-  } else if (string === "I love you, Grandma.") {
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
-  }
+  } else if (string === string.toLowerCase()){
+return "I can't hear you!"
+  } return "YES INDEED!"
 }
-
 sayHiToGrandma ("hello")
