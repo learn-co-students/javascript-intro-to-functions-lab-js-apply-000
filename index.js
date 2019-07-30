1cada7559f87cff1208d695207f6accee3365a90
James Sharpe <jameswrightsharpe@gmail.com>
2019-07-30 22:41:40 +0000

    Done.

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ function logWhisper(string){
   console.log(whisper(string));
 }
 
-function sayHiToGrandma(message){
-  if (message.toLowerCase === message) {return "I can\'t hear you!"}
-  if (message.toUpperCase === message) {return "YES INDEED!"}
-  if (message === "I love you, Grandma") {return "I love you, too."}
+function sayHiToGrandma(string){
+  if (string.toLowerCase === string) {return "I can\'t hear you!"}
+  if (string.toUpperCase === string) {return "YES INDEED!"}
+  if (string === "I love you, Grandma") {return "I love you, too."}
+  return "I can\'t hear you!"
+
 }
