a41deeed48ea4114035b44ee81ae54890d758c9d
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-08-09 01:49:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function LogShout(string) {
+ console.log(spy); string.toLowerCase(); 
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+}
