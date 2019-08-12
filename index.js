6b03e05cf41d87c3e3eea635301aee4d5e7ff901
Soumya Goyal <somyagoyal739@gmail.com>
2019-08-12 23:07:53 +0000

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function sayHiToGrandma(string){
   } else if (string.toUpperCase() === string){
     return "YES INDEED!";
   } else if (string === "I love you, Grandma."){
-    return "I love you, too.";
+     return "I love you, too.";
   }
 }
