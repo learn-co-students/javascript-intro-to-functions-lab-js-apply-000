e28c54f6faae07d9c00b2b4569d6ca65ef9048a7
Ryan Odegard <rsodegard@gmail.com>
2019-09-11 22:10:51 +0000

    Done.

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log('HELLO')
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma(string){
   if (string === string.toLowerCase())
-  return "I can\'t hear you!";
+    return "I can\'t hear you!"
   else if (string === string.toUpperCase())
-  return "YES INDEED!";
-  else if (string === "I love you, Grandma.")
-  return "I love you, too.";
-  
+    return "YES INDEED!"
+  else if(string === "I love you, Grandma.")
+    return "I love you, too."
 }
-
-sayHiToGrandma("I love you, Grandma.");
