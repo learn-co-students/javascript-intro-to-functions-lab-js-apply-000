63a94e4a785e6ff3f313fb398321102782f67d34
Muna Abdelrazeq <mma207@scarletmail.rutgers.edu>
2019-09-15 04:01:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string)
+{return string.toUpperCase()}
+function whisper(string)
+{return string.toLowerCase()}
+function logShout(string)
+{const s = string.toUpperCase()
+console.log(s)}
+function logWhisper(string)
+{const w = string.toLowerCase()
+console.log(w)}
+function sayHiToGrandma(string)
+{if (string.toLowerCase() == string)
+return "I can't hear you!"}
+function sayHiToGrandma(string)
+{if (string.toUpperCase() == string)
+return "YES INDEED!"}
+function sayHiToGrandma(string)
+{if ("I love you, Grandma." == string) 
+return "I love you, too."}
+
