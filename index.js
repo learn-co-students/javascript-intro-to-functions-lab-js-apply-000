065e482b63213a5c4a75a18bf62638c595dcca6b
victorious-firmware-2064 <alanna.nuala@gmail.com>
2019-10-09 15:42:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const logShout = (str) => {
+const shout = str => {
+  return str.toUpperCase()
+}
+
+const logShout = str => {
   console.log(str.toUpperCase())
+}
+
+const whisper = str => {
+  return str.toLowerCase()
+}
+
+const logWhisper = str => {
+  console.log(str.toLowerCase())
+}
+
+const sayHiToGrandma = str => {
+  if (str === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      return 'YES INDEED!'
+    } else if (str[i] === str[i].toLowerCase()) {
+      return 'I can\'t hear you!'
+    }
+  }
 }
