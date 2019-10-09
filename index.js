c741e4500eb56c565cec9ce0ccddbdfffdbf41ef
victorious-firmware-2064 <alanna.nuala@gmail.com>
2019-10-09 15:25:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-function shout(string) {
-  return string.toUpperCase()
+const logShout = (str) => {
+  console.log(str.toUpperCase())
 }
-
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function sayHiToGrandma(string) {
-  function whisper() {
-  var lowercase = 'hello!'
-  lowercase.toLowerCase() === lowercase;
-  return "I can't hear you!";
-    function shout() {
-    var uppercase = "HELLO!"
-      uppercase.toUpperCase() === uppercase;
-      return "YES INDEED!"
-        function iLoveYou(string) { 
-          var mixedCase = 'I love you, Grandma.'
-            mixedCase.toLowerCase() === mixedCase 
-            mixedCase.toUpperCase() === mixedCase
-            return "I love you, too."
-}
-}
-}
-}
-console.log(sayHiToGrandma());
