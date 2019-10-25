66550b4e200f1cdb8a4bb1a72a05afde4b7ad7c2
Sam Hoidal <sjhoidal@gmail.com>
2019-10-25 21:24:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-
+//return argument to all uppercase using toUpperCase method.
 function shout(string) {
   return string.toUpperCase()
 }
 
+//return argument to all lowercase using toLowerCase method.
 function whisper(string) {
   return string.toLowerCase()
 }
 
+//logs argument to the console in all uppercase using toUpperCase method.
 function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+//logs argument to console in all lowercase using toLowerCase method.
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+//define uppercase and lowercase globally to be used in following function.
 var uppercase = 'HELLO'
 var lowercase = "hello"
+
+/*use conditional (else if) function to return strings when strict equality to if statement is true. This passed the tests with specific strings. 
+
+  Instead of defining each variable with a string, could this be done by defining the variables with a method to evaluate any string value (ie. hi instead of hello) for capitalization when executing function?
+*/
 
 function sayHiToGrandma(string) {
   if ((string) === lowercase) {
