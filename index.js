b64b0f81c73860a1a00188582b020a58c6abfbd3
Rob Lawrence <iamrobnyce@gmail.com>
2019-12-27 21:45:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 function shout(string) {
   return string.toUpperCase()
 }
-  var uppercase = "HELLO!"
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+var uppercase=string.toUpperCase();
+string=uppercase;
+console.log (uppercase);
+}
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = lowercase;
+  console.log(lowercase);
+}
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
+}
+uppercase.toUpperCase() === uppercase //
 
-uppercase.toUpperCase(sayHiToGrandma) === uppercase //
 
 var lowercase = 'hello!'
 
-lowercase.toLowerCase(sayHiToGrandma) === lowercase //
+lowercase.toLowerCase() === lowercase //
 
 var mixedCase = 'Hi there!'
 
-mixedCase.toLowerCase(sayHiToGrandma) === mixedCase //
+mixedCase.toLowerCase() === mixedCase //
 
-mixedCase.toUpperCase(sayHiToGrandma) === mixedCase //
+mixedCase.toUpperCase() === mixedCase //
