d56cbe3e5d9867ac5f994ce754ad30713fc4bd5f
Moshe Telesh <mtelesh30@gmail.com>
2020-01-17 01:26:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string)
+{
+  return "hello".toUpperCase()
+}
+function whisper(string){return "hello"
+  
+}
+function logShout(string){                              
+  console.log("HELLO")
+
+}
+function logWhisper(string){console.log("hello") 
+}
+
+function sayHiToGrandma(string){
+  if(string==="I love you, Grandma."){return "I love you, too."}
+  else if(string===string.toLowerCase()){
+    return "I can't hear you!"}
+  else if(string===string.toUpperCase()){return "YES INDEED!"}}
+
+
+
+
