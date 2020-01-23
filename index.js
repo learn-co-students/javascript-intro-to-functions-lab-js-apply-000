b9d1b4796d40943f76c883ac2a17c823591b7f42
Michael Jiang <jiangm1@yahoo.com>
2020-01-23 03:41:52 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === string){
+  
+  if (string === string.toLowerCase()){
     return "I can't hear you!";
   }
   
@@ -24,6 +25,6 @@ function sayHiToGrandma(string){
   }
   
   if (string === "I love you, Grandma."){
-    return "I love you, too.";
+    return "I love you, too."
   }
 }
