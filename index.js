46df179ee84788213fd98f7e2a26ae86065a4cfc
Markus Williams <markuswilliamsdev@gmail.com>
2020-01-31 21:41:39 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
 
-  if (string.toUpperCase() === string) {
+  if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
 
