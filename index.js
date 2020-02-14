2ef350b86ed409c60a91bc705ac8ae0db217c803
Jansen Fink <jansenfink@charter.net>
2020-02-14 19:11:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+ console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+ console.log(string.toLowerCase());
+}
+
+var lowercase = "I can't hear you!";
+
+var uppercase = "YES INDEED!";
+
+var mixedCase = "I love you, too.";
+
+
+function sayHiToGrandma(string) {  
+    if (string.toLowerCase() === string) {
+    return lowercase;
+  }
+ if  (string.toUpperCase() === string)  {
+    return uppercase;
+  }
+
+ else {
+  return mixedCase;
+}
+  
+}
