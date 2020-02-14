7f68b0e7fef4851772c565449fe41fe336a0b7c5
Ian McKenzie <imckenzie777@gmail.com>
2020-02-14 21:59:44 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
-  } else if (string.toUpperCase) {
-    return "YES INDEED!"
+  } else {
+    return "yes indeed!".toUpperCase();
   }
 }
