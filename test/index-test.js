7c3271bd0517ee37bf2da99270d1550dd8517800
Adrien <asheldon12@gmail.com>
2020-02-20 01:37:10 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -35,16 +35,16 @@ describe('logWhisper(string)', function() {
   })
 })
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
