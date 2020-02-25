4b8d4522ba706e865e7596a18df1ad83864118d4
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 21:15:57 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!"; 
   } else if (string === string.toUpperCase()){
     return "YES INDEED!";
-  } else if ( "I love you, Grandma." === string){
+  } else if ( string === "I love you, Grandma."){
   return "I love you, too.";
   }
 }
