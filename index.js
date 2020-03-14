2cc0ba96edbd44348966dec23153bb8d31b18406
Christopher Boykin <biznessmain@gmail.com>
2020-03-14 20:24:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout (string){
+  return string.toUpperCase();
+}
+
+function whisper (string){
+  return string.toLowerCase();
+}
+
+function logShout (string){
+console.log( string.toUpperCase());
+}
+
+function logWhisper (string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  string === toLowerCase();
+ console.log ("i can\'t hear you!") ;
+  string === toUpperCase();
+  console.log( "YES INDEED!");
+  string === "I love you, Grandma.";
+  console.log ("I love you, too.");
+}
+
