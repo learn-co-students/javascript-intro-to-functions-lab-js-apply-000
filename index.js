37055099b0ddfc7f556b61e8e3a02873402daf22
Isabella Kirsten Mighell <ikm4dm@virginia.edu>
2020-03-14 00:32:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string){
+  
+  return string.toUpperCase()
+  
+}
+
+function whisper(string){
+  
+  return string.toLowerCase()
+  
+}
+
+function logShout(string){
+  
+  console.log(shout(string))
+  
+}
+
+function logWhisper(string){
+  
+  console.log(whisper(string))
+  
+}
+
+
+function sayHiToGrandma(string){
+
+if(string.toLowerCase() === string) 
+  return "I can't hear you!"
+
+  
+else if (string.toUpperCase()=== string )
+  return "YES INDEED!"
+
+ 
+else("I love you, Grandma." === string )
+  return "I love you, too."
+  
+}
