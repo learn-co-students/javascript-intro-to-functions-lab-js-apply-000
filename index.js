b8bc8ad815db1c9daac3a976aee5906ad34ad6ca
Nunny <vsnunny@gmail.com>
2020-03-29 00:20:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+  let final = string.toUpperCase();
+  return final;
+  
+}
+
+function whisper(string) {
+  let secret = string.toLowerCase();
+  return secret;
+  
+}
+
+function logShout(string) {
+  let printShout = console.log(string.toUpperCase());
+  return print;
+
+}
+
+function logWhisper(string) {
+  let printWhisper = console.log(string.toLowerCase());
+  return printWhisper;
+}
+
+function sayHiToGrandma(string) {
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  
+  if(string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  
+  if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
+  
+}
+
+
+
+
+
+
