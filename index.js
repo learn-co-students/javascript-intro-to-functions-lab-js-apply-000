1401d0d21c5a8d744001c1e7bb397fd363ed8041
kabine kaba <kabine92@gmail.com>
2020-04-09 23:04:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(hello) {
+  return hello.toUpperCase( );
+}
+
+function whisper(HELLO) {
+  return HELLO.toLowerCase( );
+}
+
+function logShout(hello) {
+  console. log(hello.toUpperCase( ));
+}
+
+function logWhisper (HELLO) {
+  console. log(HELLO.toLowerCase( ));
+}
+
+function sayHiToGrandma (hello) {
+  var cantHear = "I can\'t hear you!";
+  var canHear = "YES INDEED!";
+  var loveYou = "I love you, too.";
+  if (hello.toLowerCase( ) === hello) {
+    return cantHear;
+  }
+  else if (hello.toUpperCase( ) === HELLO) {
+    return canHear;
+  }
+  else if ("I love you, Grandma." === I love you, Grandma.) {
+    return loveYou
+  }
+}
+
