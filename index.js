7c9d4700e7629c82b77b880c4b128017161df173
Victor Romero <romeroa.victor@yahoo.com>
2020-05-01 01:03:19 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if string.toLowerCase()=string{
-   return "I can't hear you"
-  }
+  if (string===string.toLowerCase()){
+    return "I can't hear you!"}
+    if(string===string.toUpperCase()){
+      return "YES INDEED!"}
+      if(string==="I love you, Grandma."){
+      return "I love you, too."
+
+}        
 }
+
