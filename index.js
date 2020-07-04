d5722c6a438f67558e65a756f355f7b309f04b38
essential-form-0157 <vasmith430@gmail.com>
2020-07-04 02:22:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout (string) {
+  return string. toUpperCase()
+}
+function whisper (string) {
+  return string. toLowerCase()
+}
+function logShout (string) {
+  console.log (string.toUpperCase( ))  
+}
+function logWhisper (string) {
+  console.log (string.toLowerCase( ))
+}
+function sayHiToGrandma (string){
+  var icanthearyou = "I can't hear you!";
+  var yesindeed = "YES INDEED!";
+  var loveyoutoo = "I love you, too.";
+  if (string.toLowerCase(string) ==string){
+    return icanthearyou;
+  }
+  else if (string.toUpperCase(string) ==string){
+    return yesindeed;
+  }
+  else if ("I love you, Grandma." ==string){
+    return loveyoutoo;
+  }
+}
