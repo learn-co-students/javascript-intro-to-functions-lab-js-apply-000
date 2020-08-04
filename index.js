7de67b85c70ce6c29dab095a54d5b3e5b261bed1
Christina Poole <codenamedchris@gmail.com>
2020-08-04 19:14:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){
+  return string.toUpperCase('hello')
+}
+
+function whisper(string){
+  return string.toLowerCase('HELLO')
+}
+
+function logShout(string){
+  console.log(string.toUpperCase('hello'))
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase('HELLO'))
+}
+
+function sayHiToGrandma(string) {
+  for (string.toLowerCase()) {
+  return "I can't hear you!"
+  (string.toUpperCase())
+  return "YES INDEED!"
+  (string === "I love you, Grandma")
+  return "I love you, too."
+  }
+}
