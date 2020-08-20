7cde7a301e41f988b3afebb5b10d3bb573764e8e
Carlos Pena <cpena@eclecticspiritsllc.com>
2020-08-20 16:26:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+const whisper = (string) => string.toLowerCase()
+
+const logShout = (string) => {
+  console.log(string.toUpperCase());
+}
+
+const logWhisper = (string) => {
+  console.log(string.toLowerCase())
+}
+
+const sayHiToGrandma = (string) => {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+      return "YES INDEED!"
+    }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
