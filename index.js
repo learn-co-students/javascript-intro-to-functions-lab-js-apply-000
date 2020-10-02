9d40e38b130ae5ec6bea12a77677a69e8aab52f7
Oluwatosin Awosika <tosin2019@gmail.com>
2020-10-02 20:40:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma (word) {
+var deaf = "I can't hear you!"
+var yes = "YES INDEED!";
+var love = "I love you, too.";
+if (word.toLowerCase(word) === word) {
+ return deaf;
+}
+else if (word.toUpperCase(word) === word) {
+  return yes;
+}
+else if ("I love you, Grandma." === word) {
+  return love;
+}
+
+  }
