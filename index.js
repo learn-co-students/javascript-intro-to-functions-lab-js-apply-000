e598e748a2bb5ee50ea79799995787fe39200b0d
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-10-18 01:34:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  let result;
+  if (string === string.toLowerCase())
+  {result = "I can\'t hear you!";}
+  if (string === string.toUpperCase())
+  {result = "YES INDEED!";}
+  if (string === "I love you, Grandma.")
+  {result = "I love you, too.";}
+  return result}
